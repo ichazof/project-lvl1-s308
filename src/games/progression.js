f342785ab2e06675be18b70cae484be476821f65
Ilya Chazov <ichazof@ya.ru>
2018-07-13 12:35:39 -0400

    add new game

---
+++ src/games/progression.js
@@ -0,0 +1,27 @@
+import gameProcess from '../game';
+import { random, makeGameGenerator } from '../utils';
+
+const lenOfProgression = 10;
+const task = 'What number is missing in this progression?';
+const maxStartMember = 50;
+const maxStep = 30;
+const gameGenerator = () => {
+  const startMember = random(maxStartMember);
+  const step = random(maxStep);
+  const randomPosition = random(10);
+  let progression = '';
+  let answer = 0;
+  for (let i = 1; i <= lenOfProgression; i += 1) {
+    if (i === randomPosition) {
+      progression += '.. ';
+      answer = startMember + (i * step);
+    } else {
+      progression += `${startMember + (i * step)} `;
+    }
+  }
+  return makeGameGenerator(progression, answer);
+};
+
+export default () => {
+  gameProcess(task, gameGenerator);
+};
